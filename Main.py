a188af665013510c6f4ca284316927053969d399
Atlas168 <BEVBIALKE@ICLOUD.COM>
2025-11-12 21:09:30 -0800

    updated default location

--- Main.py
+++ Main.py
@@ -11,16 +11,17 @@ from RequestManager import RequestManager
 
 #USER INFO HERE:
 #Hour difference compared to UTC - Google this
-offset = -7
+offset = 0
 
 #Latitude, Longitude, and Elevation of location - Google this
-latitude = 47.6740 #degrees North
-longitude = -122.1215 #degrees East
-altitude = 13 #meters above sea level
+latitude = 0 #degrees North
+longitude = 0 #degrees East
+altitude = 0 #meters above sea level
 
 
 #Nothing below needs to be edited
 location = EarthLocation(lat=latitude*u.deg, lon=longitude*u.deg, height=altitude*u.m)
 
 rm = RequestManager(offset, location)
+
 rm.plot()
